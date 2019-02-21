151dfee76bb2372491372b743c1ce9954ba326ec
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-21 18:38:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+	return drivers.filter(function(driver)
+  {
+		if (driver.revenue > revenue)
+    {
+      data = ({name:driver.name, revenue:driver.revenue})
+      return data;
+		}
+	})
+};
+
+function driverNamesWithRevenueOver(driver, revenue)
+{
+  result = driversWithRevenueOver(driver, revenue);
+  return result.map(driver => driver.name);
+}
+
+function exactMatch(driver, object)
+{
+  if (object.hasOwnProperty("name")){
+    return driver.filter((drivers) => {
+      return drivers.name === object["name"];
+    });
+  }
+
+  else {
+    return driver.filter((drivers) => {
+      return drivers.revenue === object["revenue"];
+    });
+  }
+}
+
+function exactMatchToList(driver, object)
+{
+  result = exactMatch(driver, object);
+  return result.map(driver => driver.name);
+}
